cea8c6d061c51d7f54c5df003cb24713c9f96ff2
Mézz <ysaline.franquet@gmail.com>
2021-02-21 14:10:26 +0100

    Mise en place de l'API Laravel pour les categories et les ressources

--- platz_project/app/Http/Controllers/Categories.php
+++ platz_project/app/Http/Controllers/Categories.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categorie;
 
 class Categories extends Controller
 {
-    //
+    public function index() {
+      return response()->json(Categorie::all());
+    }
 }
--- platz_project/routes/api.php
+++ platz_project/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Categories;
+use App\Http\Controllers\Ressources;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// API des Categories
+// CTRL : Categories
+Route::resource('categories', Categories::class)->except(['show', 'create', 'edit']);
+
+// API des Ressources
+// CTRL : Ressources
+Route::resource('ressources', Ressources::class)->except(['show', 'create', 'edit']);
